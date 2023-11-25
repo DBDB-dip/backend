09623905f1617269c7c1a6dcdd4b1b1654f61997
Gahee Kim <zedy.dev@mynail.kr>
2023-11-25 17:13:12 +0900

    fix : 전문가 Repository Methods 추가

--- src/main/java/dbdip/demo/expert/repository/ExpertsRepository.java
+++ src/main/java/dbdip/demo/expert/repository/ExpertsRepository.java
@@ -3,5 +3,20 @@ package dbdip.demo.expert.repository;
 import dbdip.demo.expert.entity.Experts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ExpertsRepository extends JpaRepository<Experts, Integer> {
+    List<Experts> findByField(String field);
+
+    List<Experts> findByPrice(Double price);
+
+    List<Experts> findByCity(String city);
+
+    List<Experts> findByFieldAndPrice(String field, Double price);
+
+    List<Experts> findByFieldAndCity(String field, String city);
+
+    List<Experts> findByPriceAndCity(Double price, String city);
+
+    List<Experts> findByFieldAndPriceAndCity(String field, Double price, String city);
 }
