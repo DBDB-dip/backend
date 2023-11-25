ea3d143b2b13e48d0f805d456033dedf2324def4
Gahee Kim <zedy.dev@mynail.kr>
2023-11-25 20:10:13 +0900

    feat : 사용자 예약 정보 날짜 기준 조회 레포지토리 로직 수정 및 쿼리 추가

--- src/main/java/dbdip/demo/reservation/repository/ReservationRepository.java
+++ src/main/java/dbdip/demo/reservation/repository/ReservationRepository.java
@@ -3,13 +3,22 @@ package dbdip.demo.reservation.repository;
 import dbdip.demo.reservation.entity.Reservation;
 import dbdip.demo.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
     List<Reservation> findAllByUsers(Users users);
-    List<Reservation> findAllByUsersBeforeToday(Users users);
-    List<Reservation> findAllByUsersAfterToday(Users users);
+    @Query(value = "SELECT ExpertID, ReservDate, ReservTime " +
+            "FROM reservation WHERE userid = :usersId " +
+            "AND ReservDate <= :today")
+    List<Reservation> findAllByUsersBeforeToday(@Param("usersId") Integer usersId, @Param("today") LocalDate today);
+    @Query(value = "SELECT ExpertID, ReservDate, ReservTime " +
+            "FROM reservation WHERE userid = :usersId " +
+            "AND ReservDate > :today")
+    List<Reservation> findAllByUsersAfterToday(@Param("usersId") Integer usersId, @Param("today") LocalDate today);
 
 }
