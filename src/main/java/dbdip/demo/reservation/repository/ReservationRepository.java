7ac55db09ddf690435e650f125709235d7bdf27f
Gahee Kim <zedy.dev@mynail.kr>
2023-11-25 19:54:03 +0900

    feat : 사용자 예약 정보 전부 조회 레포지토리 로직 생성

--- src/main/java/dbdip/demo/reservation/repository/ReservationRepository.java
+++ src/main/java/dbdip/demo/reservation/repository/ReservationRepository.java
@@ -1,9 +1,13 @@
 package dbdip.demo.reservation.repository;
 
 import dbdip.demo.reservation.entity.Reservation;
+import dbdip.demo.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+    List<Reservation> findAllByUsers(Users users);
+
 }
--- src/main/java/dbdip/demo/reservation/repository/ReviewRepository.java
+++ src/main/java/dbdip/demo/reservation/repository/ReviewRepository.java
@@ -4,9 +4,11 @@ import dbdip.demo.expert.entity.Experts;
 import dbdip.demo.reservation.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
     @Query(value = "SELECT Review.*, Experts.Eid\n" +
             "FROM Review, Reservation, Experts\n" +
