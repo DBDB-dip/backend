a21d7411df64e6751fcd49033d22d44813895d7a
Gahee Kim <zedy.dev@mynail.kr>
2023-11-28 16:22:50 +0900

    fix : add annotaiton

--- src/main/java/dbdip/demo/tarot/repository/TarotCardDescriptionRepository.java
+++ src/main/java/dbdip/demo/tarot/repository/TarotCardDescriptionRepository.java
@@ -2,6 +2,8 @@ package dbdip.demo.tarot.repository;
 
 import dbdip.demo.tarot.entity.TarotCardDescription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TarotCardDescriptionRepository extends JpaRepository<TarotCardDescription, Integer> {
 }
