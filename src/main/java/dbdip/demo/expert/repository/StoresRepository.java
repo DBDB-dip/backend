4db75e88d1f222f130d570e5e2af297c12e0e5b0
Gahee Kim <zedy.dev@mynail.kr>
2023-11-28 16:23:00 +0900

    fix : add annotaiton

--- src/main/java/dbdip/demo/expert/repository/StoresRepository.java
+++ src/main/java/dbdip/demo/expert/repository/StoresRepository.java
@@ -2,6 +2,8 @@ package dbdip.demo.expert.repository;
 
 import dbdip.demo.expert.entity.Stores;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StoresRepository extends JpaRepository<Stores, Integer> {
 }
